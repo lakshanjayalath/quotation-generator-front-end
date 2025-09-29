78ab28ec3249fb82699f2bc59e2ed0eaacaea445
lakshanjayalath <iamdhananjaya119@gmail.com>
2025-09-29 21:53:14 +0530

    feat: add landing page component and update routing in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import NewItemForm from './components/NewItemForm'
 import ItemPage from './components/ItemPage'
 import Dashboard from './pages/Dashboard'
+import NewQuotationForm from './components/NewQuotationForm'
+import LandingPage from './pages/LandingPage'
 
 function App() {
 
@@ -10,10 +12,11 @@ function App() {
 
       <div>
         <Routes>
-          <Route path='/' element={<h1>Home Page</h1>} />
+          <Route path='/' element={<LandingPage />} />
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/items' element={<ItemPage />} />
           <Route path='/new-item' element={<NewItemForm />} />
+          <Route path='/new-quote' element={<NewQuotationForm />} />
         </Routes>
       </div>
 
